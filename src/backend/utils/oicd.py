a227403e00dd9ba79526824cea0cff50ae938426
charles <charles.englebert@protonmail.com>
2025-12-10 15:31:31 +0100

    ✨(backend) implement FindRagBackend
    We want to be able to use Find api in rag tools.
    I add a new rag backend class to do so.

---
+++ src/backend/utils/oicd.py
@@ -0,0 +1,26 @@
+import requests
+from django.conf import settings
+from lasuite.oidc_login.backends import get_oidc_refresh_token, store_tokens
+
+
+def refresh_access_token(session):
+    """Refresh the OIDC access token using the refresh token."""
+    response = requests.post(
+        settings.OIDC_OP_TOKEN_ENDPOINT,
+        data= {
+            "grant_type": "refresh_token",
+            "client_id": settings.OIDC_RP_CLIENT_ID,
+            "client_secret": settings.OIDC_RP_CLIENT_SECRET,
+            "refresh_token": get_oidc_refresh_token(session),
+        },
+        timeout=5
+    )
+    response.raise_for_status()
+    token_info = response.json()
+
+    store_tokens(
+        session,
+        access_token=token_info.get("access_token"),
+        id_token=None,
+        refresh_token=token_info.get("refresh_token")
+    )
