daf90cf110200bb5f69860a344657ecc2678cc9e
Laurent Paoletti <lp@providenz.fr>
2025-12-23 16:40:51 +0100

    ⚰️(back) remove dead code and unused files
    Signed-off-by: Laurent Paoletti <lp@providenz.fr>

--- src/backend/core/filters.py
+++ src/backend/core/filters.py
@@ -2,31 +2,9 @@
 
 import unicodedata
 
-import django_filters
-
 
 def remove_accents(value):
     """Remove accents from a string (vélo -> velo)."""
     return "".join(
         c for c in unicodedata.normalize("NFD", value) if unicodedata.category(c) != "Mn"
     )
-
-
-class AccentInsensitiveCharFilter(django_filters.CharFilter):
-    """
-    A custom CharFilter that filters on the accent-insensitive value searched.
-    """
-
-    def filter(self, qs, value):
-        """
-        Apply the filter to the queryset using the unaccented version of the field.
-
-        Args:
-            qs: The queryset to filter.
-            value: The value to search for in the unaccented field.
-        Returns:
-            A filtered queryset.
-        """
-        if value:
-            value = remove_accents(value)
-        return super().filter(qs, value)
